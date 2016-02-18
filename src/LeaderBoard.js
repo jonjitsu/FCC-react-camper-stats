47313da4f81039fc745b9ad065dd8b7156960211
Jonathan Willis <jonathanwillis@linux.com>
2016-02-18 12:49:10 -0500

    Added rows to leaderboard

--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -1,17 +1,49 @@
 import React from 'react';
 
+const TOP30=1,
+      ALLTIME=2,
+
+      sortByField = field => {
+          return (u1, u2) => {
+              if( u1[field] > u2[field] ) return -1;
+              if( u1[field] < u2[field] ) return 1;
+              return 0;
+          };
+      },
+      sortByRecent = sortByField('recent'),
+      sortByAlltime = sortByField('alltime')
+;
+
 class LeaderBoard extends React.Component {
+    loadFromServer() {
+        require('./leader-board-service.js')()
+            .all()
+            .then((data)=>{
+                this.data = data;
+                this.updateBoard();
+            });
+    }
+    updateBoard() {
+        let sorter = this.sortBy===TOP30 ? sortByRecent : sortByAlltime,
+            newState = {leaders: this.data.sort(sorter).slice(0,100)};
+
+        this.setState(newState);
+    }
     constructor() {
         super();
-
         this.state = {leaders:[]};
+        this.sortBy = TOP30;
+        this.loadFromServer();
     }
     render() {
+        const leaders = this.state.leaders.map((leader,i)=>{
+            return (<LeaderBoardRow key={leader.username} count={i+1} name={leader.username} recent={leader.recent} alltime={leader.alltime} />);
+        });
         return (
             <div>
-            <LeaderBoardHeader />
-                <LeaderBoardRow count="1" name="Jimbo" recent="556" alltime="1233" />
-                </div>
+                <LeaderBoardHeader />
+                {leaders}
+            </div>
         );
     }
 }
